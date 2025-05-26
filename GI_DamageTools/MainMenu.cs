d335208c52638e3691e93a446f2c1ad322b4a149
shine-sys <115991829+shine-sys@users.noreply.github.com>
2025-05-26 19:30:27 +0900

    ライセンス表記を追加

---
+++ GI_DamageTools/MainMenu.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GI_DamageTools
+{
+    public class Menu
+    {
+        public static void SelectMenu()
+        {
+            Console.Clear();
+            writelogo();
+            Console.WriteLine("[");
+            Console.WriteLine("1");
+            Console.WriteLine("] ダメージ計算");
+        }
+
+        public static void writelogo()
+        {
+            string logo = @" _  _ 
+                                                                                                                      
+                                                                                                                      
+    //   ) )                                                                   /__  ___/                              
+   //            ___         __        ___       / __       ( )       __         / /         ___        ___       //  
+  //  ____     //___) )   //   ) )   ((   ) )   //   ) )   / /     //   ) )     / /        //   ) )   //   ) )   //   
+ //    / /    //         //   / /     \ \      //   / /   / /     //   / /     / /        //   / /   //   / /   //    
+((____/ /    ((____     //   / /   //   ) )   //   / /   / /     //   / /     / /        ((___/ /   ((___/ /   //     
+";
+
+
+            Console.WriteLine(logo);
+        }
+    }
+}
